2078c3659609011c17b3f0b4d06c4ff12df18913
redystum <rffarffarffa@gmail.com>
2025-11-06 15:20:28 +0000

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="p-6">
+    <h1 class="text-2xl text-red-800">You did it!</h1>
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+
+</style>
