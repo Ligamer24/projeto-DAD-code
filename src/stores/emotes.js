d97a73b663a4e5d3bc575f7148f3b8bc6a871a6c
Rúben Alves <2231034@my.ipleiria.pt>
2026-01-03 15:48:02 +0000

    more hihihiha

--- src/stores/emotes.js
+++ src/stores/emotes.js
@@ -6,6 +6,8 @@ export const useEmotesStore = defineStore("emotes", () => {
     const EMOTES = [
         {id: 1, image: "hihihiha.gif", sound: 'hihihiha.mp3'},
         {id: 2, image: "dog.gif", sound: 'dog.mp3'},
+        {id: 3, image: "pedro.gif", sound: 'pedro.mp3'},
+        {id: 4, image: "mamaco.gif", sound: 'mamaco.mp3'},
     ];
 
     const getEmotes = () => {
